17d57b2d84641ea1e805234316292fdaeb7c02c8
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-09-28 11:25:46 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for row in range(n):
+        current_row = [1]
+        if row > 0:
+            previous_row = triangle[row - 1]
+            for col in range(1, row):
+                current_row.append(previous_row[col - 1] + previous_row[col])
+            current_row.append(1)
+        triangle.append(current_row)
+
+    return triangle
